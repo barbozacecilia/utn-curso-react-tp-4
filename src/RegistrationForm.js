1137efdd66adc2dc2b89f42e39249d5973bec846
barbozacecilia <barboza.ceciliam@gmail.com>
2022-03-20 16:18:24 -0300

    first commit

---
+++ src/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+class RegistrationForm extends Component {
+    render(){
+        return (
+            <>
+            Hi Universe!
+            </>
+        )
+    }
+}
+
+export default RegistrationForm;
